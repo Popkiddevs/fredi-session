6b9c46273dcfa26c79712e5b29c870ad5498c8d3
Popkid <popkidtechs@gmail.com>
2025-01-22 12:18:49 +0300

    ⭕POP KID⭕

--- pair.js
+++ pair.js
@@ -57,23 +57,23 @@ router.get('/', async (req, res) => {
                let session = await Pair_Code_By_Fredie_Tech.sendMessage(Pair_Code_By_Fredie_Tech.user.id, { text: '' + b64data });
 
                let FREDI_TECH_TEXT = `
-✧LUCKY MD DEVICE SUCCESSFUL CONNECTED✧
+✞︎𝐏𝐎𝐏𝐊𝐈𝐃 𝐌𝐃 𝐒𝐄𝐒𝐒𝐈𝐎𝐍𝐒✞︎
 ▬▬▬▬▬▬▬▬▬▬▬▬▬▬
-🤦  Creator ☞ ✧FREDI EZRA✧
+🤦  Creator ☞ ✞︎𝐏𝐎𝐏𝐊𝐈𝐃✞︎
 ▬▬▬▬▬▬▬▬▬▬▬▬▬▬
-🫂  WhattsApp Channel ☞ https://whatsapp.com/channel/0029VasiOoR3bbUw5aV4qB31
+🫂  WhattsApp Channel ☞ https://whatsapp.com/channel/0029VadQrNI8KMqo79BiHr3l
 ▬▬▬▬▬▬▬▬▬▬▬▬▬▬
-🥂 Contact Owne ☞ https://wa.me/255752593977
-FREDIETECH
+🥂 Contact Owne ☞ https://wa.me/254732297194
+POPKIDTECH
 ▬▬▬▬▬▬▬▬▬▬▬▬▬▬
-> ©*FREDIE TECH 2025 SCRIPT*
+> ©*POPKID TECH 2025 SCRIPT*
 
 _Don't Forget To Give Star To My Repo_`
- await Pair_Code_By_Fredie_Tech.sendMessage(Pair_Code_By_Fredie_Tech.user.id,{text:FREDI_TECH_TEXT},{quoted:session})
+ await Pair_Code_By_Popkid_Tech.sendMessage(Pair_Code_By_popkidTech.user.id,{text:POPKID_TECH_TEXT},{quoted:session})
  
 
         await delay(100);
-        await Pair_Code_By_Fredie_Tech.ws.close();
+        await Pair_Code_By_Popkid_Tech.ws.close();
         return await removeFile('./temp/'+id);
             } else if (connection === "close" && lastDisconnect && lastDisconnect.error && lastDisconnect.error.output.statusCode != 401) {
                     await delay(10000);
